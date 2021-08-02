7b2076c35b8573594fc8bf3e4030aef4e4610e44
Jan C. Brammer <jan.c.brammer@gmail.com>
2021-08-02 15:01:23 +0000

    Supply command line options to CLI.

--- cli_app.rb
+++ cli_app.rb
@@ -1,4 +1,12 @@
 require './inchi_v1.4'
+require 'optparse'
+
+# run as `ruby cli_app.rb --molfile=<path/to/molefile>`
+
+options = {}
+OptionParser.new do |opt|
+  opt.on('--molfile MOLFILE') { |o| options[:molfile] = o }
+end.parse!
 
 # label2 = FXLabel.new(self, "Sum formula: none")
 # label3 = FXLabel.new(self, "Output format: none")
@@ -9,12 +17,11 @@ require './inchi_v1.4'
 puts 'A new International Chemical Identifier (nInChI)'
 puts 'CC BY-SA | Ulrich Schatzschneider | Universität Würzburg | NFDI4Chem | v1.4 | 06/2021'
 
-puts 'Please enter a file name:'
-filename = gets.chomp
+filename = options[:molfile]
 file = read_molfile(filename)
 
 molecule = create_molecule_array(file, filename)
 canonicalized_molecule = canonicalize_molecule(molecule)
-ninchi_string = create_ninchi_string(canonicalized_molecule)
-dot_file_string = create_dot_file(canonicalized_molecule)
+create_ninchi_string(canonicalized_molecule)
+create_dot_file(canonicalized_molecule)
 puts 'Output format: DOT file - to display go to https://dreampuf.github.io/GraphvizOnline/#'

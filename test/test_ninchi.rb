f34dbcd570cc9f00d2f07228d1cd659daa4f1273
Jan C. Brammer <jan.c.brammer@gmail.com>
2021-08-11 12:33:07 +0000

    Added TPP test in order to hit `swap_logic2` method.

--- test/test_ninchi.rb
+++ test/test_ninchi.rb
@@ -44,6 +44,65 @@ class TestnInChI < Minitest::Test
   "  9 -- 10 [color=black,style=bold];\n"\
   "}\n"
 
+  tpp_molfile = read_molfile('test/testfiles/tpp.mol')
+  tpp_array = create_molecule_array(tpp_molfile)
+  @@canonicalized_tpp = canonicalize_molecule(tpp_array, @@periodic_table_elements)
+  @@ninchi_string_tpp = 'nInChI=1S/C20N4/c(0-1)(0-12)(1-13)(2-3)(2-14)(3-15)(4-6)(4-16)(5-12)(5-16)(6-17)(7-14)(7-17)(8-10)(8-18)(9-13)(9-18)(10-19)(11-15)(11-19)(12-20)(13-20)(14-21)(15-21)(16-22)(17-22)(18-23)(19-23)'
+  @@dot_file_string_tpp =  "graph test\n{\n  bgcolor=grey\n"\
+  "  0 [label=\"C 0\" color=\"#909090\",style=filled,shape=circle,fontname=Calibri];\n"\
+  "  1 [label=\"C 1\" color=\"#909090\",style=filled,shape=circle,fontname=Calibri];\n"\
+  "  2 [label=\"C 2\" color=\"#909090\",style=filled,shape=circle,fontname=Calibri];\n"\
+  "  3 [label=\"C 3\" color=\"#909090\",style=filled,shape=circle,fontname=Calibri];\n"\
+  "  4 [label=\"C 4\" color=\"#909090\",style=filled,shape=circle,fontname=Calibri];\n"\
+  "  5 [label=\"C 5\" color=\"#909090\",style=filled,shape=circle,fontname=Calibri];\n"\
+  "  6 [label=\"C 6\" color=\"#909090\",style=filled,shape=circle,fontname=Calibri];\n"\
+  "  7 [label=\"C 7\" color=\"#909090\",style=filled,shape=circle,fontname=Calibri];\n"\
+  "  8 [label=\"C 8\" color=\"#909090\",style=filled,shape=circle,fontname=Calibri];\n"\
+  "  9 [label=\"C 9\" color=\"#909090\",style=filled,shape=circle,fontname=Calibri];\n"\
+  "  10 [label=\"C 10\" color=\"#909090\",style=filled,shape=circle,fontname=Calibri];\n"\
+  "  11 [label=\"C 11\" color=\"#909090\",style=filled,shape=circle,fontname=Calibri];\n"\
+  "  12 [label=\"C 12\" color=\"#909090\",style=filled,shape=circle,fontname=Calibri];\n"\
+  "  13 [label=\"C 13\" color=\"#909090\",style=filled,shape=circle,fontname=Calibri];\n"\
+  "  14 [label=\"C 14\" color=\"#909090\",style=filled,shape=circle,fontname=Calibri];\n"\
+  "  15 [label=\"C 15\" color=\"#909090\",style=filled,shape=circle,fontname=Calibri];\n"\
+  "  16 [label=\"C 16\" color=\"#909090\",style=filled,shape=circle,fontname=Calibri];\n"\
+  "  17 [label=\"C 17\" color=\"#909090\",style=filled,shape=circle,fontname=Calibri];\n"\
+  "  18 [label=\"C 18\" color=\"#909090\",style=filled,shape=circle,fontname=Calibri];\n"\
+  "  19 [label=\"C 19\" color=\"#909090\",style=filled,shape=circle,fontname=Calibri];\n"\
+  "  20 [label=\"N 20\" color=\"#3050F8\",style=filled,shape=circle,fontname=Calibri];\n"\
+  "  21 [label=\"N 21\" color=\"#3050F8\",style=filled,shape=circle,fontname=Calibri];\n"\
+  "  22 [label=\"N 22\" color=\"#3050F8\",style=filled,shape=circle,fontname=Calibri];\n"\
+  "  23 [label=\"N 23\" color=\"#3050F8\",style=filled,shape=circle,fontname=Calibri];\n"\
+  "  0 -- 1 [color=black,style=bold];\n"\
+  "  0 -- 12 [color=black,style=bold];\n"\
+  "  1 -- 13 [color=black,style=bold];\n"\
+  "  2 -- 3 [color=black,style=bold];\n"\
+  "  2 -- 14 [color=black,style=bold];\n"\
+  "  3 -- 15 [color=black,style=bold];\n"\
+  "  4 -- 6 [color=black,style=bold];\n"\
+  "  4 -- 16 [color=black,style=bold];\n"\
+  "  5 -- 12 [color=black,style=bold];\n"\
+  "  5 -- 16 [color=black,style=bold];\n"\
+  "  6 -- 17 [color=black,style=bold];\n"\
+  "  7 -- 14 [color=black,style=bold];\n"\
+  "  7 -- 17 [color=black,style=bold];\n"\
+  "  8 -- 10 [color=black,style=bold];\n"\
+  "  8 -- 18 [color=black,style=bold];\n"\
+  "  9 -- 13 [color=black,style=bold];\n"\
+  "  9 -- 18 [color=black,style=bold];\n"\
+  "  10 -- 19 [color=black,style=bold];\n"\
+  "  11 -- 15 [color=black,style=bold];\n"\
+  "  11 -- 19 [color=black,style=bold];\n"\
+  "  12 -- 20 [color=black,style=bold];\n"\
+  "  13 -- 20 [color=black,style=bold];\n"\
+  "  14 -- 21 [color=black,style=bold];\n"\
+  "  15 -- 21 [color=black,style=bold];\n"\
+  "  16 -- 22 [color=black,style=bold];\n"\
+  "  17 -- 22 [color=black,style=bold];\n"\
+  "  18 -- 23 [color=black,style=bold];\n"\
+  "  19 -- 23 [color=black,style=bold];\n"\
+  "}\n"
+
   def test_hydrogen_ninchi_string
     assert_equal @@ninchi_string_hydrogen, create_ninchi_string(@@canonicalized_hydrogen, @@periodic_table_elements)
   end
@@ -52,11 +111,19 @@ class TestnInChI < Minitest::Test
     assert_equal @@ninchi_string_cisplatin, create_ninchi_string(@@canonicalized_cisplatin, @@periodic_table_elements)
   end
 
+  def test_tpp_ninchi_string
+    assert_equal @@ninchi_string_tpp, create_ninchi_string(@@canonicalized_tpp, @@periodic_table_elements)
+  end
+
   def test_hydrogen_dot_file_string
     assert_equal @@dot_file_string_hydrogen, create_dot_file(@@canonicalized_hydrogen, @@periodic_table_colors)
   end
 
   def test_cisplatin_dot_file_string
     assert_equal @@dot_file_string_cisplatin, create_dot_file(@@canonicalized_cisplatin, @@periodic_table_colors)
+  end
+
+  def test_tpp_dot_file_string
+    assert_equal @@dot_file_string_tpp, create_dot_file(@@canonicalized_tpp, @@periodic_table_colors)
   end
 end

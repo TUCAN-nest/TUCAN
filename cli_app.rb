a18a97805076fa9b6050eec9bb7ca61ccfb75422
Jan C. Brammer <jan.c.brammer@gmail.com>
2021-09-14 12:09:27 +0000

    Added CLI flag that enables optional random permutation of atom indices.

--- cli_app.rb
+++ cli_app.rb
@@ -3,6 +3,7 @@ require 'optparse'
 require './periodic_table'
 
 # run as `ruby cli_app.rb --molfile=<path/to/molefile>`
+# run with `--permute-input` flag in order to randomly permute the atom indices
 
 class CommandLineInterface
   include Inchi
@@ -12,8 +13,10 @@ class CommandLineInterface
     options = {}
     OptionParser.new do |opt|
       opt.on('--molfile MOLFILE') { |o| options[:molfile] = o }
+      opt.on('--permute-input') { |o| options[:permute] = o }
     end.parse!
     @filename = options[:molfile]
+    @permute = options[:permute] || false
   end
 
   def run
@@ -25,6 +28,7 @@ class CommandLineInterface
     molfile_data.each { |line| puts line }
 
     molecule = create_molecule_array(molfile_data, PeriodicTable::ELEMENTS)
+    molecule = update_molecule_indices(molecule, random_indices: true) if @permute
     canonicalized_molecule = canonicalize_molecule(molecule)
     puts "\n#{write_ninchi_string(canonicalized_molecule, PeriodicTable::ELEMENTS)}"
     puts "\n#{write_dot_file(canonicalized_molecule, PeriodicTable::ELEMENTS, PeriodicTable::ELEMENT_COLORS)}"

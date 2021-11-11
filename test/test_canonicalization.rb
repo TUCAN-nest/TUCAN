a8d95b0844fe9a63d94b951cc362cd1e9cbe568f
Ulrich Schatzschneider <88166560+schatzsc@users.noreply.github.com>
2021-11-11 03:06:17 +0100

    Update test_canonicalization.rb
    print better than puts?!?

--- test/test_canonicalization.rb
+++ test/test_canonicalization.rb
@@ -16,11 +16,11 @@ class CanonicalizationValidationTests < Minitest::Test
   # inspired by https://stackoverflow.com/questions/18770988/.
   MoleculeCollection.new.molecules.each do |molecule|
     define_method("test_canonicalization_#{molecule.name}") do
-      puts "\n#{'-' * 75}\n"
-      puts "\nNow working on: ",molecule.name,"\n"
-      puts "\nProcessing original molfile\n"
+      print "\n#{'-' * 75}\n"
+      print "\nNow working on: ",molecule.name,"\n"
+      print "\nProcessing original molfile\n"
       ninchi_string_original_molfile = molecule.ninchi_string(permute_molfile: false)
-      puts "\nProcessing permuted molfile\n"
+      print "\nProcessing permuted molfile\n"
       ninchi_string_permuted_molfile = molecule.ninchi_string(permute_molfile: true, random_seed: 181)
       assert_equal(ninchi_string_original_molfile, ninchi_string_permuted_molfile)
     end

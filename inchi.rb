9a7a330215444f043b921984029fdebbad86c8e9
Ulrich Schatzschneider <88166560+schatzsc@users.noreply.github.com>
2021-11-27 02:31:24 +0100

    Update inchi.rb

--- inchi.rb
+++ inchi.rb
@@ -265,8 +265,7 @@ def sort_by_distance(adjacency_matrix, node_features_matrix, distance_matrix) # 
         distance_A = distance_matrix[row][atom_count - 1] * node_features_matrix[row][0]
         distance_B = distance_matrix[row + 1][atom_count - 1] * node_features_matrix[row + 1][0]
         if ((node_features_matrix[row][0] == node_features_matrix[row + 1][0]) && (node_features_matrix[row][2] == node_features_matrix[row + 1][2]) && (distance_A < distance_B))
-          adjacency_matrix, node_features_matrix, distance_matrix = swap_matrix_elements(adjacency_matrix,
-                                                                                         node_features_matrix, distance_matrix, row, row + 1)
+          adjacency_matrix, node_features_matrix, distance_matrix = swap_matrix_elements(adjacency_matrix,node_features_matrix, distance_matrix, row, row + 1)
         end
       end
     end
@@ -280,8 +279,7 @@ def sort_by_distance_matrix(adjacency_matrix, node_features_matrix, distance_mat
     for j in 0..(atom_count - 2)
       for row in 0..atom_count - 2
         if ((node_features_matrix[row][0] == node_features_matrix[row + 1][0]) && (node_features_matrix[row][2] == node_features_matrix[row + 1][2]) && (distance_matrix[row].to_s > distance_matrix[row + 1].to_s))
-          adjacency_matrix, node_features_matrix, distance_matrix = swap_matrix_elements(adjacency_matrix,
-                                                                                         node_features_matrix, distance_matrix, row, row + 1)
+          adjacency_matrix, node_features_matrix, distance_matrix = swap_matrix_elements(adjacency_matrix,node_features_matrix, distance_matrix, row, row + 1)
         end
       end
     end
@@ -299,20 +297,19 @@ def sort_terminal_hydrogens(adjacency_matrix, node_features_matrix, distance_mat
   end
   print "\nNumber of terminal H atoms: ",number_of_terminal_hydrogens,"\n"
   for i in 0..number_of_terminal_hydrogens-1
-    #print "\nIteration: ",i,"\n"
+    print "\nIteration: ",i,"\n"
     for row in 0..number_of_terminal_hydrogens-1
       neighbour_A = 0
       neighbour_B = 0
       for column in 0..atom_count-1
         if(adjacency_matrix[row][column] != 0)
-         adjacency_matrix, node_features_matrix, distance_matrix = sort_terminal_hydrogens(adjacency_matrix, node_features_matrix, distance_matrix)        
-         neighbour_A = column
+          neighbour_A = column
         end
         if(adjacency_matrix[row+1][column] != 0)
           neighbour_B = column
         end
       end
-      #print "(",neighbour_A,":",neighbour_B,") "
+      print "(",neighbour_A,":",neighbour_B,") "
       if(neighbour_A > neighbour_B)
         adjacency_matrix, node_features_matrix, distance_matrix = swap_matrix_elements(adjacency_matrix, node_features_matrix, distance_matrix, row, row+1)
       end
@@ -334,8 +331,7 @@ def sort_adjacency_matrix(adjacency_matrix, node_features_matrix, distance_matri
   print "\nNow sorting adjacency matrix\n\n"
   print_adjacency_matrix(adjacency_matrix, node_features_matrix, distance_matrix)
   print "\nNumber of atoms: #{atom_count}\n"
-  adjacency_matrix, node_features_matrix, distance_matrix = sort_by_element_and_connectivity(adjacency_matrix,
-                                                                                             node_features_matrix, distance_matrix)
+  adjacency_matrix, node_features_matrix, distance_matrix = sort_by_element_and_connectivity(adjacency_matrix, node_features_matrix, distance_matrix)
   adjacency_matrix, node_features_matrix, distance_matrix = sort_by_connectivity_index(adjacency_matrix, node_features_matrix, distance_matrix)
   
   #adjacency_matrix, node_features_matrix, distance_matrix = sort_by_distance(adjacency_matrix, node_features_matrix, distance_matrix)

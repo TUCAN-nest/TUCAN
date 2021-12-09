b87bcffdd32dc24452d361c097d6f96950e1e431
Ulrich Schatzschneider <88166560+schatzsc@users.noreply.github.com>
2021-12-09 07:30:08 +0100

    Update inchi.rb (#38)
    * Update inchi.rb
    
    Now hopefully finally implemented the correct "optimizing bubble sort" in sort_by_element_and_connectivity()
    https://en.wikipedia.org/wiki/Bubble_sort
    
    * Update inchi.rb
    
    the connectivity_index does not need to be calculated again in sort_by_connectivity_index() as it is an invariant of the graph and already stored when setting up the data structure in node_features_matrix[row][3], therefore innermost for loop can be removed
    
    * Update inchi.rb
    
    since connectivity_index is an invariant which does not need to be re-calculated, sort_by_connectivity_index() can be turned into a simple "bubble sort" - and possibly somehow even be added to the "binary sorting" used in sort_by_element_and_connectivity() to save some looping
    
    * Update inchi.rb
    
    in sort_by_distance() make sure entries with similar distance index are not mixed up again - also pretty sure the number of loops can be reduced here as the distance_matrix is also an invariant only calculated once when setting up the data structure, but this will be the subject of the next code update

--- inchi.rb
+++ inchi.rb
@@ -194,35 +194,28 @@ def calculate_connectivity_index(adjacency_matrix, node_features_matrix, distanc
   connectivity_index
 end
 
-def calculate_ac_index(node_features_matrix) # ac_index is a combination of atomic number and number of edges weighted by row number
-  atom_count = node_features_matrix.length
-  ac_index = 0
-  for row in 0..(atom_count - 1)
-    ac_index += node_features_matrix[row][0] * (row + 1) + node_features_matrix[row][2] * (row + 1)
-  end
-  ac_index
-end
+#
+# sort by equivalence class
+#
 
 def sort_by_element_and_connectivity(adjacency_matrix, node_features_matrix, distance_matrix)
   print "\nNow sorting by atomic number and connectivity: \n"
-  atom_count = node_features_matrix.length
-  iteration = 1
-  old_ac_index = 0
-  new_ac_index = calculate_ac_index(node_features_matrix)
-  while (old_ac_index < new_ac_index)
-    for row in 0..(atom_count-2)
-      a = convert_to_bin(node_features_matrix[row][0],8)+"-"+convert_to_bin(node_features_matrix[row][2],5)
-      b = convert_to_bin(node_features_matrix[row+1][0],8)+"-"+convert_to_bin(node_features_matrix[row+1][2],5)
-      print a,"\n"
-      print b,"\n"
-      if(a > b)
+  count = node_features_matrix.length
+  iteration = 0
+  loop do
+    swapped = false
+    for row in 0..(count-2)
+      atom_a = convert_to_bin(node_features_matrix[row][0],8)+"-"+convert_to_bin(node_features_matrix[row][2],5)
+      atom_b = convert_to_bin(node_features_matrix[row+1][0],8)+"-"+convert_to_bin(node_features_matrix[row+1][2],5)
+      if(atom_a > atom_b)
         adjacency_matrix, node_features_matrix, distance_matrix = swap_matrix_elements(adjacency_matrix, node_features_matrix, distance_matrix, row, row+1)
+        swapped = true
       end
     end
-    old_ac_index = new_ac_index
-    new_ac_index = calculate_ac_index(node_features_matrix)
-    print "\nIteration ",iteration,": ",old_ac_index," -> ",new_ac_index,"\n"
+    count -= 1
     iteration += 1
+    print "\nIteration ",iteration," \n"
+    break if (swapped == false)
   end
   print "\n"
   print_adjacency_matrix(adjacency_matrix, node_features_matrix, distance_matrix)
@@ -231,34 +224,29 @@ end
 
 def sort_by_connectivity_index(adjacency_matrix, node_features_matrix, distance_matrix) # sort by connectivity index
   print "\nNow sorting by connectivity index: \n"
-  iteration = 1
-  converged = false
-  atom_count = node_features_matrix.length
-  previous_molecule_states = [Marshal.load(Marshal.dump(adjacency_matrix))]
-  until converged == true
-    print "\nIteration: #{iteration}\n"
-    for row in 0..atom_count - 2
-      for column in 0..atom_count - 1
-        node_features_matrix[row][3] =
-          calculate_connectivity_index(adjacency_matrix, node_features_matrix, distance_matrix, row)
-        node_features_matrix[row + 1][3] =
-          calculate_connectivity_index(adjacency_matrix, node_features_matrix, distance_matrix, row + 1)
+    count = node_features_matrix.length
+    iteration = 0
+    loop do
+      swapped = false
+      for row in 0..count-2
+        if((node_features_matrix[row][0] == node_features_matrix[row+1][0]) && (node_features_matrix[row][1] == node_features_matrix[row+1][1]) && (node_features_matrix[row][3] > node_features_matrix[row+1][3]))
+          adjacency_matrix, node_features_matrix, distance_matrix = swap_matrix_elements(adjacency_matrix, node_features_matrix, distance_matrix, row, row+1)
+          swapped = true
+        end
       end
-      if ((node_features_matrix[row][0] == node_features_matrix[row + 1][0]) && (node_features_matrix[row][1] == node_features_matrix[row + 1][1]) && (node_features_matrix[row][3] > node_features_matrix[row + 1][3]))
-        adjacency_matrix, node_features_matrix, distance_matrix = swap_matrix_elements(adjacency_matrix,
-                                                                                       node_features_matrix, distance_matrix, row, row + 1)
-      end
+      count -= 1
+      iteration += 1
+      print "\nIteration ",iteration," \n"
+      break if (swapped == false)
     end
-    if (previous_molecule_states.include?(adjacency_matrix))
-      converged = true
-    end
-    previous_molecule_states.push(Marshal.load(Marshal.dump(adjacency_matrix)))
-    iteration += 1
-  end
   print "\n"
   print_adjacency_matrix(adjacency_matrix, node_features_matrix, distance_matrix)
   [adjacency_matrix, node_features_matrix, distance_matrix]
 end
+
+#
+# sort by symmetry class and symmetry order
+#
 
 def sort_by_distance(adjacency_matrix, node_features_matrix, distance_matrix) # sort by distance to highest priority atom
   print "\nNow sorting by distance: \n\n"
@@ -268,7 +256,7 @@ def sort_by_distance(adjacency_matrix, node_features_matrix, distance_matrix) # 
       for row in 0..atom_count-2
         distance_A = distance_matrix[row][atom_count-1]
         distance_B = distance_matrix[row+1][atom_count-1]
-        if((node_features_matrix[row][0] == node_features_matrix[row+1][0]) && (node_features_matrix[row][2] == node_features_matrix[row+1][2]) && (distance_A < distance_B))
+        if((node_features_matrix[row][0] == node_features_matrix[row+1][0]) && (node_features_matrix[row][2] == node_features_matrix[row+1][2]) && (node_features_matrix[row][3] == node_features_matrix[row+1][3]) && (distance_A < distance_B))
           adjacency_matrix, node_features_matrix, distance_matrix = swap_matrix_elements(adjacency_matrix, node_features_matrix, distance_matrix, row, row+1)
           print "."
         end

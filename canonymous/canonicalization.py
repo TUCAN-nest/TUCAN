e8f825503c6282280755bccad07a85b2c7485eb9
Jan C. Brammer <jan.c.brammer@gmail.com>
2022-02-16 15:33:58 +0000

    Serialization: write sum formula according to Hill and sort node-labels by atomic number.

--- canonymous/canonicalization.py
+++ canonymous/canonicalization.py
@@ -273,25 +273,36 @@ def canonicalize_molecule(m, root_idx=0):
 def serialize_molecule(m):
     """Serialize a molecule."""
     serialization = _sum_formula(m)
-    for edge in sorted([sorted(edge) for edge in m.edges()]):
+    m_sorted_by_atomic_number = _sort_molecule_by_attribute(m, "atomic_number")
+    for edge in sorted([sorted(edge) for edge in m_sorted_by_atomic_number.edges()]):
         serialization += f"/{edge[0]}-{edge[1]}"
     return serialization
 
 
 def _sum_formula(m):
-    """Write sum formula of a molecule.
+    """Write the sum formula of a molecule.
 
-    Elements occur in the following order: C, H, other elements in alphabetic order.
+    In the sum formula the elements are ordered according to Hill system [1]:
+    1. C
+    2. H
+    3. symbols of remaining elements in alphabetic order (including H in other
+    than carbon compounds)
+
+    References
+    ----------
+    [1] doi:10.1021/ja02046a005
     """
     element_counts = Counter(nx.get_node_attributes(m, "element_symbol").values())
     element_counts = {
         k: (v if v > 1 else "") for k, v in element_counts.items()
     }  # remove counts of 1 since those are implicit in sum formula
     sum_formula = ""
-    for element in ["C", "H"]:
-        count = element_counts.pop(element, None)
-        if count:
-            sum_formula += f"{element}{count}"
+    carbon_count = element_counts.pop("C", None)
+    if carbon_count:
+        sum_formula += f"C{carbon_count}"
+        hydrogen_count = element_counts.pop("H", None)
+        if hydrogen_count:
+            sum_formula += f"H{hydrogen_count}"
     for k, v in dict(sorted(element_counts.items())).items():
         sum_formula += f"{k}{v}"
     return sum_formula

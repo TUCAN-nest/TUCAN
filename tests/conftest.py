1bf5957013c623c237001440278f1fc44f9a56ae
Jan C. Brammer <jan.c.brammer@gmail.com>
2022-02-24 17:24:04 +0000

    Added command line option for skipping disconnected graphs during tests.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,26 @@
 import pytest
 from pathlib import Path
+from networkx.algorithms.components import is_connected
+from canonymous.canonicalization import graph_from_file
 
 
 def pytest_addoption(parser):
     parser.addoption(
         "--testset", action="store", default="mol", help="testset: one of {mol, cfi}"
     )
+    parser.addoption(
+        "--only-connected-graphs", action="store_true"
+    )  # defaults to False
 
 
 def pytest_configure(config):
     testset_cmd = config.getoption("--testset")
-    pytest.testset = None
+    only_connected_graphs_cmd = config.getoption("--only-connected-graphs")
+    testset = None
     if testset_cmd == "mol":
-        pytest.testset = list(Path("tests/molfiles").glob("*/*.mol"))
+        testset = list(Path("tests/molfiles").glob("*/*.mol"))
     elif testset_cmd == "cfi":
-        pytest.testset = list(Path("tests/cfi_rigid_benchmark_graphs").glob("*.col"))
+        testset = list(Path("tests/cfi_rigid_benchmark_graphs").glob("*.col"))
+    if only_connected_graphs_cmd:
+        testset = [m for m in testset if is_connected(graph_from_file(m))]
+    pytest.testset = testset

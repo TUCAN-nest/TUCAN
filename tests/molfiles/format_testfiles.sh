46776b2105ff1e93a0aad68b1c23caab996f8615
Jan C. Brammer <jan.c.brammer@gmail.com>
2022-02-24 16:19:50 +0000

    Added molecules from AK Herres-Pawlis.

--- tests/molfiles/format_testfiles.sh
+++ tests/molfiles/format_testfiles.sh
@@ -2,10 +2,15 @@
 
 # Need to use quotation marks oround paths since they can contain spaces.
 
+for mol2path in ./Data/*/*.mol2 ; do
+    mol2file=${mol2path##*/}
+    molname=${mol2file%%.mol2}
+    obabel -imol2 "$mol2path" -omol -O "./${molname}.mol" -x3
+done
+
 shopt -s extglob # enables use of patterns when stripping `moldata` to obtain `molname`
 shopt -s nullglob # don't execute loop in case there's no matching `moldata`
-
-for moldata in  ./*.mol ./*_metadata.txt ./*_drawing.*; do
+for moldata in ./*.mol ./*_metadata.txt ./*_drawing.*; do
     molname=${moldata%@(.mol|_metadata.txt|_drawing.*)}
     if [ ! -d "$molname" ]; then
         echo "Making ${molname} directory."

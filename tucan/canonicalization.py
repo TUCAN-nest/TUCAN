2c3b0d2d2fdfffe7dec82deb05404fa19618845c
Jan C. Brammer <jan.c.brammer@gmail.com>
2022-03-16 18:47:18 +0000

    Factored out instantiation of graph.

--- tucan/canonicalization.py
+++ tucan/canonicalization.py
@@ -8,9 +8,23 @@ import random
 def graph_from_file(filepath):
     if filepath.suffix == ".mol":
         element_symbols, bonds = _parse_molfile(filepath)
-    if filepath.suffix == ".col":
+    elif filepath.suffix == ".col":
         element_symbols, bonds = _parse_dimacs(filepath)
-    element_colors = [ELEMENT_PROPS[s]["element_color"] for s in element_symbols]
+    else:
+        raise IOError("Invalid file format, must be one of {.mol, .col}.")
+    return _graph_from_moldata(element_symbols, bonds)
+
+
+def _graph_from_moldata(element_symbols, bonds):
+    """Instantiate a NetworkX graph from molecular data.
+
+    Parameters
+    ----------
+    element_symbols: List[str]
+        Element symbols associated with the atoms.
+    bonds: List[Tuple[int, int]]
+        Bonds between atoms.
+    """
     atomic_numbers = [ELEMENT_PROPS[s]["atomic_number"] for s in element_symbols]
     node_labels = range(len(element_symbols))
     graph = nx.Graph()
@@ -18,9 +32,6 @@ def graph_from_file(filepath):
     graph.add_edges_from(bonds)
     nx.set_node_attributes(
         graph, dict(zip(node_labels, element_symbols)), "element_symbol"
-    )
-    nx.set_node_attributes(
-        graph, dict(zip(node_labels, element_colors)), "element_color"
     )
     nx.set_node_attributes(
         graph, dict(zip(node_labels, atomic_numbers)), "atomic_number"
@@ -42,10 +53,17 @@ def _parse_molfile(filepath):
     element_symbols = [
         l[3] for l in lines[atom_block_offset : atom_block_offset + atom_count]
     ]
+    assert (
+        len(element_symbols) == atom_count
+    ), f"Number of atoms {len(element_symbols)} doesn't match atom-count specified in header {atom_count}."
     bonds = [
         (int(l[4]) - 1, int(l[5]) - 1)
         for l in lines[bond_block_offset : bond_block_offset + bond_count]
     ]  # make bond-indices zero-based
+    assert (
+        len(bonds) == bond_count
+    ), f"Number of bonds {len(bonds)} doesn't match bond-count specified in header {bond_count}."
+
     return element_symbols, bonds
 
 

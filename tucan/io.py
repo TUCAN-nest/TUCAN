bcf200a72f49f8658572cc366c44ed48b275f280
Jan C. Brammer <jan.c.brammer@gmail.com>
2022-06-30 16:46:23 +0000

    Parse additional atom properties from molfile. Some refactoring.

--- tucan/io.py
+++ tucan/io.py
@@ -1,81 +1,100 @@
 import networkx as nx
+from pathlib import Path
 from tucan.element_properties import ELEMENT_PROPS
-from typing import List, Tuple
+from typing import Dict, List, Tuple
 
 
-def graph_from_file(filepath):
-    with open(filepath) as f:
-        filecontent = f.read()
-    if filepath.suffix == ".mol":
-        node_labels, element_symbols, bonds = _parse_molfile3000(filecontent)
-    elif filepath.suffix == ".col":
-        node_labels, element_symbols, bonds = _parse_dimacs(filecontent)
-    else:
-        raise IOError("Invalid file format, must be one of {.mol, .col}.")
-    return graph_from_moldata(node_labels, element_symbols, bonds)
-
-
-def graph_from_moldata(
-    node_labels: List[int], element_symbols: List[str], bonds: List[Tuple[int]]
-):
-    """Instantiate a NetworkX graph from molecular data.
+def graph_from_file(filepath: str) -> nx.Graph:
+    """Instantiate a NetworkX graph from an MDL molfile.
 
     Parameters
     ----------
-    node_labels: List[int]
-        Node labels associated with the atoms.
-    element_symbols: List[str]
-        Element symbols associated with the atoms.
-    bonds: List[Tuple[int, int]]
-        Bonds between atoms.
+    filepath: str
+        Path pointing to a file containing a v3000 MDL (now BIOVIA) connection
+        table [1].
+
+    Returns
+    -------
+    NetworkX Graph
+
+    References
+    ----------
+    [1] https://en.wikipedia.org/wiki/Chemical_table_file
     """
-    atomic_numbers = [ELEMENT_PROPS[s]["atomic_number"] for s in element_symbols]
+    filepath = Path(filepath)
+    if filepath.suffix != ".mol":
+        raise IOError(f"The file must be in '.mol' format, not {filepath.suffix}.")
+    filecontent = _read_file(filepath)
+
+    atom_props = _parse_atom_block_molfile3000(filecontent)
+    bonds = _parse_bond_block_molfile3000(filecontent)
+
     graph = nx.Graph()
-    graph.add_nodes_from(node_labels)
+    graph.add_nodes_from(list(atom_props.keys()))
+    nx.set_node_attributes(graph, atom_props)
     graph.add_edges_from(bonds)
-    nx.set_node_attributes(
-        graph, dict(zip(node_labels, element_symbols)), "element_symbol"
-    )
-    nx.set_node_attributes(
-        graph, dict(zip(node_labels, atomic_numbers)), "atomic_number"
-    )
-    nx.set_node_attributes(graph, 0, "partition")
+
     return graph
 
 
-def _parse_molfile3000(filecontent: str):
-    lines = [l.rstrip().split(" ") for l in filecontent.splitlines()]
-    lines = [[value for value in line if value != ""] for line in lines]
+def _read_file(filepath: str) -> List[List[str]]:
+    with open(filepath) as file:
+        filecontent = file.read()
+    lines = [line.rstrip().split(" ") for line in filecontent.splitlines()]
+
+    return [[value for value in line if value != ""] for line in lines]
+
+
+def _parse_atom_block_molfile3000(lines: List[List[str]]) -> Dict:
+    atom_count = int(lines[5][3])
+    atom_block_offset = 7
+
+    atom_props = {
+        int(line[2])
+        - 1: _parse_atom_props(line)  # map zero-based label to property-dict
+        for line in lines[atom_block_offset : atom_block_offset + atom_count]
+    }
+    assert len(atom_props) == atom_count, (
+        f"Number of atoms {len(atom_props)} doesn't match atom-count specified in"
+        f" header {atom_count}."
+    )
+
+    return atom_props
+
+
+def _parse_atom_props(line: List[str]) -> Dict:
+    chg = [int(i.split("=")[1]) for i in line if "CHG" in i]
+    mass = [int(i.split("=")[1]) for i in line if "MASS" in i]
+    rad = [int(i.split("=")[1]) for i in line if "RAD" in i]
+
+    atom_props = {
+        "element_symbol": line[3],
+        "atomic_number": ELEMENT_PROPS[line[3]]["atomic_number"],
+        "partition": 0,
+        "x_coord": float(line[4]),
+        "y_coord": float(line[5]),
+        "z_coord": float(line[6]),
+        "chg": chg.pop() if chg else 0,
+        "mass": mass.pop() if mass else 0,
+        "rad": rad.pop() if rad else 0,
+    }
+
+    return atom_props
+
+
+def _parse_bond_block_molfile3000(lines: List[List[str]]) -> List[Tuple[int, int]]:
     atom_count = int(lines[5][3])
     bond_count = int(lines[5][4])
     atom_block_offset = 7
     bond_block_offset = atom_block_offset + atom_count + 2
-    node_labels = [
-        int(l[2]) - 1 for l in lines[atom_block_offset : atom_block_offset + atom_count]
-    ]  # convert from 1-based to 0-based
-    element_symbols = [
-        l[3] for l in lines[atom_block_offset : atom_block_offset + atom_count]
-    ]
-    assert (
-        len(element_symbols) == atom_count
-    ), f"Number of atoms {len(element_symbols)} doesn't match atom-count specified in header {atom_count}."
-    bonds = [
-        (int(l[4]) - 1, int(l[5]) - 1)
-        for l in lines[bond_block_offset : bond_block_offset + bond_count]
-    ]  # make bond-indices zero-based
-    assert (
-        len(bonds) == bond_count
-    ), f"Number of bonds {len(bonds)} doesn't match bond-count specified in header {bond_count}."
-    return node_labels, element_symbols, bonds
 
-
-def _parse_dimacs(filecontent: str):
-    lines = [l.rstrip().split(" ") for l in filecontent.splitlines()]
-    lines = [l for l in lines if l[0] in ["p", "e"]]
-    atom_count = int(lines[0][2])
-    node_labels = range(atom_count)
-    element_symbols = ["C"] * atom_count
     bonds = [
-        (int(l[1]) - 1, int(l[2]) - 1) for l in lines[1:]
+        (int(line[4]) - 1, int(line[5]) - 1)
+        for line in lines[bond_block_offset : bond_block_offset + bond_count]
     ]  # make bond-indices zero-based
-    return node_labels, element_symbols, bonds
+    assert len(bonds) == bond_count, (
+        f"Number of bonds {len(bonds)} doesn't match bond-count specified in header"
+        f" {bond_count}."
+    )
+
+    return bonds

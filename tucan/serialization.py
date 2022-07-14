a6fd9932b4a7a9d4c6693a53f952caa0adb5bb93
Jan C. Brammer <jan.c.brammer@gmail.com>
2022-07-14 07:03:47 +0000

    Append additional node properties to serialization.

--- tucan/serialization.py
+++ tucan/serialization.py
@@ -5,18 +5,41 @@ import networkx as nx
 
 def serialize_molecule(m):
     """Serialize a molecule."""
-    serialization = _sum_formula(m)
-    m_sorted_by_atomic_number = sort_molecule_by_attribute(m, "atomic_number")
-    for edge in _edge_list(m_sorted_by_atomic_number):
-        serialization += f"/{edge[0] + 1}-{edge[1] + 1}"
+    m_sorted = sort_molecule_by_attribute(m, "atomic_number")
+    serialization = _write_sum_formula(m_sorted)
+    serialization += f"/{_write_edge_list(m_sorted)}"
+    node_properties = _write_node_properties(m_sorted)
+    serialization += f"/{node_properties}" if node_properties else ""
+
     return serialization
 
 
-def _edge_list(m):
-    return sorted([sorted(edge) for edge in m.edges()])
+def _write_edge_list(m):
+    sorted_edges = sorted([sorted(edge) for edge in m.edges()])
+    edge_list_string = "".join(
+        [f"({edge[0] + 1}-{edge[1] + 1})" for edge in sorted_edges]
+    )
+
+    return edge_list_string
 
 
-def _sum_formula(m):
+def _write_node_properties(m):
+    node_properties = ["chg", "mass", "rad"]
+    node_property_string = ""
+    for node in sorted(m.nodes(data=True)):
+        label, props = node
+        available_props = [
+            f"{prop}={props[prop]}" for prop in node_properties if prop in props
+        ]
+        if not available_props:
+            continue
+        node_property_string += f"({label}:"
+        node_property_string += f"{','.join(available_props)})"
+
+    return node_property_string
+
+
+def _write_sum_formula(m):
     """Write the sum formula of a molecule.
 
     In the sum formula the elements are ordered according to Hill system [1]:
@@ -33,13 +56,14 @@ def _sum_formula(m):
     element_counts = {
         k: (v if v > 1 else "") for k, v in element_counts.items()
     }  # remove counts of 1 since those are implicit in sum formula
-    sum_formula = ""
+    sum_formula_string = ""
     carbon_count = element_counts.pop("C", None)
     if carbon_count:
-        sum_formula += f"C{carbon_count}"
+        sum_formula_string += f"C{carbon_count}"
         hydrogen_count = element_counts.pop("H", None)
         if hydrogen_count:
-            sum_formula += f"H{hydrogen_count}"
+            sum_formula_string += f"H{hydrogen_count}"
     for k, v in dict(sorted(element_counts.items())).items():
-        sum_formula += f"{k}{v}"
-    return sum_formula
+        sum_formula_string += f"{k}{v}"
+
+    return sum_formula_string

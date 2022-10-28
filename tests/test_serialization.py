e6e44e91d7eae35aa15fe051c8b5adda076c172c
flange-ipb <34936695+flange-ipb@users.noreply.github.com>
2022-10-28 15:48:29 +0200

    use syrupy instead of snapshottest

--- tests/test_serialization.py
+++ tests/test_serialization.py
@@ -3,5 +3,11 @@ from tucan.canonicalization import canonicalize_molecule
 
 
 def test_regression(m, snapshot):
+    """
+    Generate TUCAN strings for all test molfiles and match them against the
+    snapshot file tests/__snapshots__/test_serialization.ambr.
+
+    Run `pytest --snapshot-update` to update the snapshot file.
+    """
     m_serialized = serialize_molecule(canonicalize_molecule(m))
-    snapshot.assert_match(m_serialized)
+    assert m_serialized == snapshot

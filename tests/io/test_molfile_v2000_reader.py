d59a1da597f16ea0dd114562f0cfadaf857c3544
flange-ipb <34936695+flange-ipb@users.noreply.github.com>
2022-11-10 11:10:33 +0100

    tests for V2000 Molfile reader

---
+++ tests/io/test_molfile_v2000_reader.py
@@ -0,0 +1,121 @@
+import pytest
+from pathlib import Path
+from tucan.io import graph_from_file
+from tucan.io.exception import MolfileParserException
+from tucan.io.molfile_v2000_reader import (
+    _merge_tuples_into_additional_props,
+    _parse_atom_value_assignments,
+    _to_int,
+    _to_float,
+)
+
+
+@pytest.mark.parametrize(
+    "mol",
+    list(map(lambda path: path.stem, Path("tests/molfiles_v2000").glob("*/*.mol"))),
+)
+def test_graphs_from_v2000_and_v3000_molfiles_match(mol):
+    graph_v2000 = graph_from_file(f"tests/molfiles_v2000/{mol}/{mol}.mol")
+    graph_v3000 = graph_from_file(f"tests/molfiles/{mol}/{mol}.mol")
+
+    assert graph_v2000.nodes(data=True) == graph_v3000.nodes(data=True)
+
+    # NetworkX's EdgeDataView objects cannot be matched directly
+    for e1, e2 in zip(
+        graph_v2000.edges(data=True), graph_v3000.edges(data=True), strict=True
+    ):
+        assert e1 == e2
+
+
+@pytest.mark.parametrize(
+    "tuples, additional_props, expected_additional_props_after_merge",
+    [
+        (
+            [
+                (0, 2),
+                (1, 13),
+                (2, 3),  # atom index is not in additional_props yet
+            ],
+            {
+                0: {"chg": 2},  # will add new key
+                1: {"mass": 1},  # will overwrite value
+            },
+            {
+                0: {"chg": 2, "mass": 2},
+                1: {"mass": 13},
+                2: {"mass": 3},
+            },
+        ),
+    ],
+)
+def test_merge_tuples_into_additional_props(
+    tuples, additional_props, expected_additional_props_after_merge
+):
+    _merge_tuples_into_additional_props(tuples, "mass", additional_props)
+    assert additional_props == expected_additional_props_after_merge
+
+
+@pytest.mark.parametrize(
+    "line, atom_props, expected_tuples",
+    [
+        (
+            "M  CHG  2  10   2   2 -15",
+            {
+                1: {},
+                9: {},
+            },
+            [
+                (9, 2),
+                (1, -15),
+            ],
+        ),
+    ],
+)
+def test_parse_atom_value_assignments(line, atom_props, expected_tuples):
+    assert _parse_atom_value_assignments(line, atom_props) == expected_tuples
+
+
+@pytest.mark.parametrize(
+    "line, atom_props, expected_error_msg",
+    [
+        (
+            "M  CHG  1   5   2",
+            {},
+            r'Unknown atom index 5 in line "M  CHG  1   5   2"',
+        ),
+    ],
+)
+def test_parse_atom_value_assignments_unknown_atom_index_raises_exception(
+    line, atom_props, expected_error_msg
+):
+    with pytest.raises(
+        MolfileParserException,
+        match=expected_error_msg,
+    ):
+        _parse_atom_value_assignments(line, atom_props)
+
+
+@pytest.mark.parametrize(
+    "s, expected_result",
+    [
+        ("  12 ", 12),
+        (" -2    ", -2),
+        ("   ", 0),
+        ("", 0),
+    ],
+)
+def test_to_int(s, expected_result):
+    assert _to_int(s) == expected_result
+
+
+@pytest.mark.parametrize(
+    "s, expected_result",
+    [
+        ("   3.14 ", 3.14),
+        (" -123.456    ", -123.456),
+        ("   ", 0),
+        ("", 0),
+    ],
+)
+def test_to_float(s, expected_result):
+    assert _to_float(s) == expected_result

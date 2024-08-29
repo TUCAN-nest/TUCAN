0e8a28dc6bacc8f34587883f740d3fd9d2aace93
Jan C. Brammer <jan.c.brammer@gmail.com>
2024-08-29 12:06:33 +0000

    Implement refinement-tree

--- tucan/canonicalization.py
+++ tucan/canonicalization.py
@@ -1,8 +1,8 @@
 from tucan.graph_attributes import PARTITION, INVARIANT_CODE
 from tucan.graph_utils import attribute_sequence
 import networkx as nx
-from igraph import Graph as iGraph
 from typing import Generator
+from collections import Counter, deque
 
 
 def partition_molecule_by_attribute(m: nx.Graph, attribute: str) -> nx.Graph:
@@ -26,8 +26,11 @@ def get_number_of_partitions(m: nx.Graph) -> int:
     return len(set(nx.get_node_attributes(m, PARTITION).values()))
 
 
-def refine_partitions(m: nx.Graph) -> Generator[nx.Graph, None, None]:
+def partitioning_is_discrete(m):
+    return get_number_of_partitions(m) == m.number_of_nodes()
 
+
+def refine_partitions(m: nx.Graph) -> Generator[nx.Graph, None, None]:
     m_refined = partition_molecule_by_attribute(m, PARTITION)
 
     if get_number_of_partitions(m_refined) == get_number_of_partitions(m):
@@ -38,35 +41,70 @@ def refine_partitions(m: nx.Graph) -> Generator[nx.Graph, None, None]:
     yield from refine_partitions(m_refined)
 
 
-def assign_canonical_labels(m: nx.Graph) -> dict[int, int]:
-    """Canonicalize node-labels of a graph.
+def get_refinement_tree_node_children(m: nx.Graph) -> Generator[nx.Graph, None, None]:
+    n_partitions = get_number_of_partitions(m)
+    partitions = nx.get_node_attributes(m, PARTITION)
+    partition_sizes = Counter(sorted(partitions.values()))
+    largest_partition = max(partition_sizes, key=partition_sizes.get)  # type: ignore
 
-    The canonical labels are computed using the igraph [1] implementation of
-    the "bliss" algorithm [2].
+    for atom, partition in m.nodes(data=PARTITION):
+        if partition_sizes[partition] == 1:
+            # No need to artificially split singleton partitions.
+            continue
+        if partition != largest_partition:
+            continue
 
-    Returns
-    -------
-    dict
-        From old labels (keys) to canonical labels (values).
+        # Split largest partition.
+        m_artificially_split = m.copy()
+        nx.set_node_attributes(
+            m_artificially_split,
+            {atom: n_partitions},  # Partitions are zero-based.
+            PARTITION,
+        )
+        m_artificially_refined = list(refine_partitions(m_artificially_split))[-1]
 
-    References
-    ----------
-    [1] https://igraph.org
-    [2] https://doi.org/10.1137/1.9781611972870.13
+        yield m_artificially_refined
+
+
+def get_discrete_partitionings(m: nx.Graph) -> Generator[nx.Graph, None, None]:
     """
+    Build BFS refinement-tree and return its leaves (i.e., discrete partitionings).
+    rtn = refinement-tree-node
+    """
+    rtn_queue = deque([m])
 
-    m_igraph = iGraph.from_networkx(m)
-    old_labels = m_igraph.vs["_nx_name"]
-    partitions = m_igraph.vs[PARTITION]
-    canonical_labels = m_igraph.canonical_permutation(color=partitions)
+    while rtn_queue:
+        rtn = rtn_queue.popleft()
 
-    return dict(zip(old_labels, canonical_labels))
+        if partitioning_is_discrete(rtn):
+            yield rtn
+            continue
+
+        rtn_queue.extend(list(get_refinement_tree_node_children(rtn)))
+
+
+def get_canonical_molecule(ms: list[nx.Graph]) -> nx.Graph:
+    m_canonical = None
+    canonical_labeling = [[0, 0]]
+
+    for m in ms:
+        m_relabeled_by_partition = nx.relabel_nodes(
+            m,
+            dict(zip(list(m), nx.get_node_attributes(m, PARTITION).values())),
+            copy=True,
+        )
+        labeling = sorted([sorted(edge) for edge in m_relabeled_by_partition.edges()])
+
+        if labeling > canonical_labeling:
+            m_canonical = m_relabeled_by_partition
+            canonical_labeling = labeling
+
+    return m_canonical
 
 
 def canonicalize_molecule(m: nx.Graph) -> nx.Graph:
     m_partitioned_by_invariant_code = partition_molecule_by_attribute(m, INVARIANT_CODE)
     m_refined = list(refine_partitions(m_partitioned_by_invariant_code))[-1]
+    ms_discrete_partitionings = list(get_discrete_partitionings(m_refined))
 
-    canonical_labels = assign_canonical_labels(m_refined)
-
-    return nx.relabel_nodes(m_refined, canonical_labels, copy=True)
+    return get_canonical_molecule(ms_discrete_partitionings)
